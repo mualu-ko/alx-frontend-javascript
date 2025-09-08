f881c1963718e6b286973185244de7bdf7f44ddc
mualu-ko <kmwaluko.wambua@gmail.com>
2025-09-08 17:09:58 +0300

    number 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,20 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200).constructor.name);   // Teacher
 console.log(createEmployee(1000).constructor.name);  // Director
 console.log(createEmployee("$500").constructor.name); // Director
+
+// Type predicate function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// executeWork function
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+// Example usage
+console.log(executeWork(createEmployee(200)));   // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
