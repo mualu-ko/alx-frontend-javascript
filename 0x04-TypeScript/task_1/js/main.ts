5c11a7c3890ccc2a8ed6f8a9e6ae7fd5fb916ec4
mualu-ko <kmwaluko.wambua@gmail.com>
2025-09-08 14:29:43 +0300

    update printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,14 +24,13 @@ const director1: Director = {
 
 console.log(director1);
 
+
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
-
+}
 
 console.log(printTeacher("John", "Doe"));
