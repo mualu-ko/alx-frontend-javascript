fa3e245f1383d100dc40f02bbeee142f40c07944
mualu-ko <kmwaluko.wambua@gmail.com>
2025-09-10 11:42:18 +0300

    Added string literals

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -74,3 +74,19 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(executeWork(createEmployee(200)));   // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+// String literal type
+type Subjects = "Math" | "History";
+
+// Function teachClass
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else {
+    return "Teaching History";
+  }
+}
+
+// Example usage
+console.log(teachClass("Math"));    // Teaching Math
+console.log(teachClass("History")); // Teaching History
